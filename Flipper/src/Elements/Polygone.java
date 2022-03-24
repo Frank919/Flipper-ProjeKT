6c2d5bfc7f70fb7d0519527c123f5c9bb71a5ecc
Frank FANG <chenglai.fang@outlook.com>
2022-03-24 09:32:02 +0100

    wow

--- Flipper/src/Elements/Polygone.java
+++ Flipper/src/Elements/Polygone.java
@@ -17,78 +17,47 @@ public class Polygone extends Obstacle{
      *      Elasticity entre 0 et 1
      */
     public Polygone(Obstacle[] obs, float s, float e){
-        /**
-         * Ranger les obs dans le sens croissant en X
-         
-        Obstacle temp = new Obstacle();
-        for(int j=0;j<obs.length-1;j++){
-            for(int i=0;i<obs.length-j-1;i++){
-                if(obs[i+1].positionX < obs[i].positionX){
-                    temp = obs[i];
-                    obs[i] = obs[i+1];
-                    obs[i+1] = temp;
-                }
-            }
-        }
-        */
-        /**
-         * Ranger les obs dans le sens croissant en Y
-         
-        for(int j=0;j<obs.length-1;j++){
-            for(int i=0;i<obs.length-j-1;i++){
-                if(obs[i+1].positionY < obs[i].positionY){
-                    temp = obs[i];
-                    obs[i] = obs[i+1];
-                    obs[i+1] = temp;
-                }
-            }
-        }
-        */
         for(int i=0;i<obs.length;i++){
             float nx = 0;
             float ny = 0;
             int max = 0;
             int min = 0;
+            Obstacle end = new Obstacle();
+            Obstacle start = new Obstacle();
             if(i == obs.length - 1){//也可以不写特例，但是在定义顶点数组时，要多加一个位置存放第一个顶点
-                float k = (obs[i].positionY - obs[0].positionY) /
-                         (obs[i].positionX - obs[0].positionX);
-                float b = obs[i].positionY - k * obs[i].positionX;
-                if(obs[i].positionX<obs[0].positionX){
-                    max = obs[0].positionX;
-                    min = obs[i].positionX;
-                }else{
-                    min = obs[0].positionX;
-                    max = obs[i].positionX;
-                }
+                end = obs[0];
+                start = obs[i];
             }else{
-                //La pente
-                int deltaY = obs[i+1].positionY - obs[i].positionY;
-                int deltaX = obs[i+1].positionX - obs[i].positionX;
-                float k = deltaY / deltaX;
-                //L'ordonée à l'origine
-                float b = obs[i].positionY - k * obs[i].positionX;
+                end = obs[i+1];
+                start = obs[i];
+            }
+            //La pente
+            int deltaY = end.positionY - start.positionY;
+            int deltaX = end.positionX - start.positionX;
+            float k = deltaY / deltaX;
+            //L'ordonée à l'origine
+            float b = start.positionY - k * start.positionX;
 
-                //Le vecteur normal
-                ny = (float)Math.sqrt(1/(1+k*k));
-                nx = - k * ny;
-                float produitVectoriel =(nx*deltaY)-(ny*deltaX);
-                
-                //Si le produit vectoriel est négatif, on change le signe de nx et ny
-                if(produitVectoriel>0){
-                    ny = -ny;
-                    nx = -nx;
-                }
-                if(obs[i].positionX<obs[i+1].positionX){
-                    max = obs[i+1].positionX;
-                    min = obs[i].positionX;
-                }else{
-                    min = obs[i+1].positionX;
-                    max = obs[i].positionX;
-                }
-                for(int x = min;x<=max;x++){
-                    int y = (int)(k*x+b);
-                    GameTable.table[x][y]=new Obstacle(nx, ny, s, e);
-                }
+            //Le vecteur normal
+            ny = (float)Math.sqrt(1/(1+k*k));
+            nx = - k * ny;
+            float produitVectoriel =(nx*deltaY)-(ny*deltaX);
+            
+            //Si le produit vectoriel est négatif, on change le signe de nx et ny
+            if(produitVectoriel>0){
+                ny = -ny;
+                nx = -nx;
+            }
+            if(start.positionX<end.positionX){
+                max = end.positionX;
+                min = start.positionX;
+            }else{
+                min = end.positionX;
+                max = start.positionX;
+            }
+            for(int x = min;x<=max;x++){
+                int y = (int)(k*x+b);
+                GameTable.table[x][y]=new Obstacle(nx, ny, s, e);
             }
         }
     }
