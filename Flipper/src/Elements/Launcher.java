5306274f3812b93b30a499c1bfbb1454172bd324
Frank FANG <chenglai.fang@outlook.com>
2022-04-07 00:44:49 +0200

    nice

--- Flipper/src/Elements/Launcher.java
+++ Flipper/src/Elements/Launcher.java
@@ -4,6 +4,9 @@ public class Launcher extends ElementKinetic {
     public Launcher(){
         nX = 0;
         nY = 1;
+        smoothness = (float)0.8;
+        elasticity = (float)0.1;
+
     }
     public Launcher(ElementBasic start, ElementBasic end){
         GameTable.table[start.positionX][start.positionY] = new Launcher();
